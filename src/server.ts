fb561fbda2f8b65f136f7b15831ed049eb90c2df
Gabo <no@email>
2025-09-18 22:53:56 +0200

    feat(deps): Intall and set fastify-multipart to handle file uploads.

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,18 @@ import { reelsRoutes } from "./modules/reels/reels.routes";
 import { taggedRoutes } from "./modules/tagged/tagged.routes";
 import { highlightsRoutes } from "./modules/highlights/highlights.routes";
 
+import multipart from "@fastify/multipart";
+import path from "path";
+import fs from "fs/promises";
+import { randomUUID } from "crypto";
+
 // creates a new Fastify instance
 const fastify = Fastify({
   logger: true,
 });
+
+// Register multipart plugin
+fastify.register(multipart);
 
 // registers the databasePlugin, which connects to the SQLite database
 // and sets up the tables and transactions helpers.
