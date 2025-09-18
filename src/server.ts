0a86124d1dd774d43d6369d3deb983b8eafe061f
Gabo <no@email>
2025-09-18 10:23:24 +0200

    feat(routes): Register tagged and highlights routes in server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import Fastify from "fastify";
 import { databasePlugin } from "./core/database/database.plugin";
 import { postsRoutes } from "./modules/posts/posts.routes";
 import { reelsRoutes } from "./modules/reels/reels.routes";
+import { taggedRoutes } from "./modules/tagged/tagged.routes";
+import { highlightsRoutes } from "./modules/highlights/highlights.routes";
 
 // creates a new Fastify instance
 const fastify = Fastify({
@@ -15,8 +17,15 @@ fastify.register(databasePlugin);
 // Register routes, making the endpoints for those modules available
 // Register posts routes
 fastify.register(postsRoutes);
+
 // Register reels routes
 fastify.register(reelsRoutes);
+
+// Register tagged routes
+fastify.register(taggedRoutes);
+
+// Register highlights routes
+fastify.register(highlightsRoutes);
 
 
 // Declare a default route to confirm the server is running.
